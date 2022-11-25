c353171659d89ebb72243a9e925aadef12ba237c
unknown <faisaljfcl@gmail.com>
2022-11-25 10:20:53 +0600

    Added Modal and Trigger on cards

--- src/components/BuyerPages/CategoryProducts.jsx
+++ src/components/BuyerPages/CategoryProducts.jsx
@@ -1,15 +1,21 @@
 const SERVER =
   import.meta.env.VITE_SERVER_ADDRESS || import.meta.env.VITE_DEV_SERVER;
 
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
-import { Card, Row, Col, Container } from "react-bootstrap";
+import { Row, Container, Modal, Button } from "react-bootstrap";
 import Loader from "../Shared/Loader";
-import moment from "moment";
+import ProductCard from "../Shared/ProductCard";
 
 export default function CategoryProducts() {
   const { categoryId } = useParams();
+
+  // Modal States
+  const [isOpenBookingModal, setIsOpenBookingModal] = useState(false);
+  const handleClose = () => setIsOpenBookingModal(false);
+  const handleOpen = () => setIsOpenBookingModal(true);
 
   const { data: categoryProducts, status } = useQuery({
     queryKey: [categoryId],
@@ -29,37 +35,43 @@ export default function CategoryProducts() {
   if (status === "loading") {
     return <Loader />;
   }
+
+  const BookingModal = () => {
+    return (
+      <>
+        <Modal show={isOpenBookingModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  };
   return (
     <div>
+      <BookingModal />
       <h1>{categoryProducts.category.category_name}</h1>
       <Container>
         <Row xs={1} md={2} lg={3}>
           {categoryProducts.products.length &&
-            categoryProducts.products.map(
-              ({ _id, photoURL, productName, description, postedTime }) => {
-                return (
-                  <Col key={_id}>
-                    {" "}
-                    <Card>
-                      <Card.Img
-                        variant="top"
-                        src={photoURL}
-                        className="img img-fluid cardImg"
-                      />
-                      <Card.Body>
-                        <Card.Title>{productName}</Card.Title>
-                        <Card.Text>{description}</Card.Text>
-                      </Card.Body>
-                      <Card.Footer>
-                        <small className="text-muted">
-                          Posted: {moment(postedTime).format("DD MMM, YYYY")}
-                        </small>
-                      </Card.Footer>
-                    </Card>
-                  </Col>
-                );
-              }
-            )}
+            categoryProducts.products.map((product) => {
+              return (
+                <ProductCard
+                  key={product._id}
+                  product={product}
+                  handleOpen={handleOpen}
+                />
+              );
+            })}
         </Row>
       </Container>
     </div>
---
+++ src/components/Shared/ProductCard.jsx
@@ -0,0 +1,30 @@
+import { Card, Col, Button } from "react-bootstrap";
+import moment from "moment";
+export default function ProductCard({ product, handleOpen }) {
+  const { _id, photoURL, productName, description, postedTime } = product;
+
+  return (
+    <Col key={_id}>
+      {" "}
+      <Card>
+        <Card.Img
+          variant="top"
+          src={photoURL}
+          className="img img-fluid cardImg"
+        />
+        <Card.Body>
+          <Card.Title>{productName}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary" onClick={handleOpen}>
+            Launch demo modal
+          </Button>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">
+            Posted: {moment(postedTime).format("DD MMM, YYYY")}
+          </small>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+}
